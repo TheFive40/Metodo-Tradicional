c39a87a4ba311ca25d766a27bb1f2c778961f8f8
Jean Franco Boom Bolaño <55298889+TheFive40@users.noreply.github.com>
2022-10-17 10:23:55 -0500

    Update HidroxidosTradicional.java

--- HidroxidosTradicional.java
+++ HidroxidosTradicional.java
@@ -2,16 +2,18 @@ package MetodoTradicional;
 
 import java.util.Scanner;
 
-public class HidroxidosTradicional {
-public void Hidroxidos() {
-	System.out.println(" -------------------------------------------------- ");
-	System.out.println("Bienvenido, al Metodo Tradicional de Nomenclatura de Hidroxidos");
-	System.out.println("¿Cual Caso para los Hidroxidos desea aplicar?");
+public class HidroxidosTradicional { //Nombre de la clase 
+public void Hidroxidos() { //Metodo que contendra nuestras lineas de codigo y que sera ejecutado al instanciar la clase Hidroxidos
+	System.out.println(" -------------------------------------------------- "); //Separador 
+	System.out.println("Bienvenido, al Metodo Tradicional de Nomenclatura de Hidroxidos"); //Mnesaje de Bienvenida
+	System.out.println("¿Cual Caso para los Hidroxidos desea aplicar?"); //Preguntamos que caso desea aplicar
 	System.out.println("\n1: Caso 1 \n2: Caso 2");
 
-	Scanner entrada = new Scanner (System.in);
-	int Caso = entrada.nextInt();
-	if(Caso == 1) {
+	Scanner entrada = new Scanner (System.in); //Hacemos un Input es decir una entrada de datos
+	int Caso = entrada.nextInt();  //Lo que ingreso por teclado lo guardamos en la variable Caso
+	if(Caso == 1) { //Si lo que hay almacenado en la Variable caso es un 1
+		
+		//Enseñamos la lista de compuestos programadores almacenados en un Array o arreglo de 5 espacio de memoria de una dimension
 		System.out.println("A continuacion la Lista de Hidroxidos Programados");
 		String [] Hidroxidos = new String [5];
 		Hidroxidos[0] = "[0] NaOH";
@@ -19,16 +21,22 @@ public void Hidroxidos() {
 		Hidroxidos[2] = "[2] MgOH";
 		Hidroxidos[3] = "[3] AlOH";
 		Hidroxidos[4] = "[4] CaOH";
+		
+		//Bucle for para recorrer dicho Arreglo 
 		for(int i = 0;i<5;i++) {
 			System.out.println(Hidroxidos[i]);
 		}
+		//Pedimos que escoja el numero de la lista que desea por teclado y nos lo guarde en la variable de tipo entero Eleccion
 		System.out.println("Por favor, escoja  el número de la lista que desea");
 		Scanner Entrada = new Scanner(System.in);
 		int Eleccion = Entrada.nextInt();
+		
+		//Evaluamos con un condicional si escogio el compuesto que esta ubicado en la posicion 0 
 		if(Eleccion == 0) {
 			System.out.println("La nomenclatura del Elemento Seleccionado es:");
 
-			System.out.println("Es un Hidroxido de Sodio O Hidroxido Sodico");
+			System.out.println("Es un Hidroxido de Sodio O Hidroxido Sodico"); //Si la condicion se cumple entonces imprime este mensaje en consola
+			//si no se cumple pasa a evaluar la siguiente condicion 
 		}else if (Eleccion == 1) {
 			System.out.println("La nomenclatura del Elemento Seleccionado es:");
 
@@ -53,7 +61,7 @@ public void Hidroxidos() {
 			System.out.println("El compuesto ingresado no está programado aún");
 		}
 		System.out.println(" -------------------------------------------------- ");
-	}else if (Caso == 2) {
+	}else if (Caso == 2) { //Si escogio el caso 2 
 		System.out.println("A continuacion la Lista de Hidroxidos Programados");
 		String [] Hidroxidos = new String [6];
 		Hidroxidos[0] = "[0] Fe2OH";
